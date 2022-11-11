4b2183c51b49a92ebdde36e1c7d7924cefd264b5
ilarioforcherio <ilarioforcherio@gmail.com>
2022-11-11 18:20:39 +0100

    sx funzionante

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,37 +2,40 @@
 var app = new Vue({
   el: '#root',
   data: {
-    
-    link:'https://picsum.photos/200/300',
-    
+
+    link: 'https://picsum.photos/200/300',
+
     obj: [
-     {img:"https://unsplash.it/300/300?image=10"} ,
-     {img: "https://unsplash.it/300/300?image=11"},
-     {img:"https://unsplash.it/300/300?image=12"},
-     ],
-    
+      { img: "https://unsplash.it/300/300?image=10" },
+      { img: "https://unsplash.it/300/300?image=11" },
+      { img: "https://unsplash.it/300/300?image=12" },
+    ],
+
     indice: 0,
   },
   methods: {
 
-    scorriImmagineDx: function(){
-       
-       if(this.indice < this.obj.length -1 ){
-         this.indice++;
-       }else{
-       this.indice = 0;
-       }
+    scorriImmagineDx: function () {
+
+      if (this.indice < this.obj.length - 1) {
+        this.indice++;
+      } else {
+        this.indice = 0;
+      }
     },
-    
-    
-    
-    scorriImmagineSx: function(){
-       
-      if(this.obj.length = this.indice){
+
+    scorriImmagineSx: function () {
+
+      if (this.indice < this.obj.length ) {
         this.indice--;
       }
-   }
-    
+      if(this.indice <= -1){
+        this.indice = 2;
+      }
+
+      
+    }
+
   }
 })
 
