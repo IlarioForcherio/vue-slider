bf05fe24b6bddc577c0da15a6c05762397ac8c9b
ilarioforcherio <ilarioforcherio@gmail.com>
2022-11-11 15:15:09 +0100

    create repository

---
+++ assets/js/main.js
@@ -0,0 +1,61 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+      obj: [
+            { id: 1, nome: 'pippo', colore: 'rosso' },
+            { id: 2, nome: 'pluto', colore: 'blue' },
+            { id: 3, nome: 'paperino', colore: 'green' }
+      ],
+      indice: 0,
+    },
+    methods: {
+      
+    }
+  })
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
